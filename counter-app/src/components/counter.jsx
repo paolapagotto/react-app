d8f874a5bf2176dd23417397ee6f972ed84bdd03
paolapagotto <paolapagotto@gmail.com>
2020-10-16 17:49:51 +0100

    updating and unmount phase

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -28,6 +28,18 @@ class Counter extends Component {
     //     this.handleIncrement({id: 1});
     // };
 
+    componentDidUpdate (prevProps, prevState) {
+        console.log('prevProps', prevProps);
+        console.log('prevState', prevState);
+        if(prevProps.counter.value !== this.props.counter.value){
+            //Ajax call and get new date from the server
+        }
+    }
+
+    componentWillUnmount(){
+        console.log('Counter - Unmount');
+    }
+
     render() { 
         console.log('Counter - Rendered');
             
