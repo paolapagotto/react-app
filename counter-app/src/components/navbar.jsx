7e76660a431d78ce7c22fa926e7881f7e0523385
paolapagotto <paolapagotto@gmail.com>
2020-10-16 17:15:41 +0100

    destructuring objects of props

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 
 //stateless functional component (sfc command):
-const NavBar = (props) => {
+const NavBar = ({ totalCounters }) => {
     return ( 
         <nav class="navbar navbar-light bg-light">
             <a class="navbar-brand" href="#">
-            Navbar <span className="badge badge-pill badge-secondary"> {props.totalCounters} </span>
+            Navbar <span className="badge badge-pill badge-secondary"> {totalCounters} </span>
             </a>
         </nav>
      );
